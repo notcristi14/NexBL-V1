5a25a3658afc711efcecaea3d233b356e6cc287a
AjaxFNC-YT <ajaxfncyt@gmail.com>
2024-12-06 16:28:22 -0600

    Add files via upload

---
+++ utils/constants.js
@@ -0,0 +1,28 @@
+module.exports.allowedPlaylists = Object.freeze([
+    "playlist_defaultduo",
+    "playlist_trios",
+    "playlist_defaultsquad",
+    "playlist_nobuildbr_trio",
+    "playlist_nobuildbr_squad",
+    "playlist_nobuildbr_duo",
+
+    "playlist_blastberrysquad",
+    "playlist_blastberryduo",
+    "playlist_blastberrytrio",
+    "playlist_blastberrynobuildsquad",
+    "playlist_blastberrynobuildduo",
+    "playlist_blastberrynobuildtrio",
+    "playlist_blastberrysolo",
+    "playlist_blastberrynobuildsolo",
+    
+    "playlist_habaneroduo",
+    "playlist_habanerosolo",
+    "playlist_habanerosquad",
+    "playlist_habanerotrio"
+])
+
+module.exports.websocketHeaders = Object.freeze({
+    'Accept-Version': '*',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache'
+})
